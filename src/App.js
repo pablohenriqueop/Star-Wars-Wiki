30ddc03a936a4560a7f6c86ccaf48870928538ad
Lucas Santos Lima <lucas@Mac-mini-de-Convidado.local>
2020-02-27 09:34:51 -0300

    add context in ListPeople

--- src/App.js
+++ src/App.js
@@ -1,31 +1,33 @@
-import { registerRootComponent } from 'expo';
-import React, {useState, useEffect} from 'react';
-if(__DEV__) {
+import { registerRootComponent } from "expo";
+import React, { useState, useEffect } from "react";
+if (__DEV__) {
   import("../ReactotronConfig");
 }
-import { StyleSheet, StatusBar, ImageBackground, ActivityIndicator} from 'react-native';
-import Header from './components/Header';
+import {
+  StyleSheet,
+  StatusBar,
+  ImageBackground,
+  ActivityIndicator
+} from "react-native";
+import Header from "./components/Header";
 
-import background from '../assets/background.jpg';
-import titulo from '../assets/titulo.png';
+import background from "../assets/background.jpg";
+import titulo from "../assets/titulo.png";
 
-import { ListPeople } from './components/ListPeople';
-import BoxPeople from './components/BoxPeople';
-import  { Title } from './components/Title';
-import api from './services/api';
-
-
-
+import { ListPeople } from "./components/ListPeople";
+import BoxPeople from "./components/BoxPeople";
+import { Title } from "./components/Title";
+import api from "./services/api";
+import ListPeopleContext from "./contexts/ListPeople";
 
 export default function App() {
   const [people, setPeople] = useState([]);
   const [arrayHolder, setArrayHolder] = useState([]);
   const [loading, setLoading] = useState(false);
 
-
   async function fechData() {
     setLoading(true);
-    const response = await api.get('/people');
+    const response = await api.get("/people");
     setLoading(false);
     setPeople(response.data.results);
     setArrayHolder(response.data.results);
@@ -36,41 +38,44 @@ export default function App() {
   }, []);
 
   return (
-    <ImageBackground style={styles.container} source={background}>
-      <StatusBar barStyle="light-content" />
-      <Title source={titulo} style={styles.strech}></Title>
-      <Header people={people} setPeople={setPeople} arrayHolder={arrayHolder}></Header>
-      {  loading ? <ActivityIndicator style={styles.actvity} size="large" color="#FFF" /> : 
-        <ListPeople
-        data={people}
-        keyExtractor={ person => person.name }
-        renderItem={({item}) => (
-          <BoxPeople
-            name={ item.name }
-            height ={ item.height }
-            gender ={ item.gender }
-            mass ={ item.mass }
-            >
-          </BoxPeople>
+    <ListPeopleContext.Provider value={{ arrayHolder, setPeople }}>
+      <ImageBackground style={styles.container} source={background}>
+        <StatusBar barStyle="light-content" />
+        <Title source={titulo} style={styles.strech}></Title>
+        <Header></Header>
+        {loading ? (
+          <ActivityIndicator style={styles.actvity} size="large" color="#FFF" />
+        ) : (
+          <ListPeople
+            data={people}
+            keyExtractor={person => person.name}
+            renderItem={({ item }) => (
+              <BoxPeople
+                name={item.name}
+                height={item.height}
+                gender={item.gender}
+                mass={item.mass}
+              ></BoxPeople>
+            )}
+          ></ListPeople>
         )}
-        ></ListPeople>
-     }
-    </ImageBackground>
+      </ImageBackground>
+    </ListPeopleContext.Provider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 20,
+    padding: 20
   },
   strech: {
     width: 200,
-    height: 150, 
-    resizeMode: 'contain',
+    height: 150,
+    resizeMode: "contain"
   },
   actvity: {
-    flex: 1,
+    flex: 1
   }
 });
 
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,24 +1,29 @@
-import React, { useState } from 'react';
+import React, { useContext } from "react";
 
-import { Feather } from '@expo/vector-icons'
-import { Container, Input} from './styles';
+import { Feather } from "@expo/vector-icons";
+import { Container, Input } from "./styles";
+import ListPeopleContext from "../../contexts/ListPeople";
 
-export default function Header({arrayHolder, setPeople}) {
+export default function Header() {
+  const { arrayHolder, setPeople } = useContext(ListPeopleContext);
 
-  searchFilterFunction = text => {   
-    const newData = arrayHolder.filter(item => {      
-       const itemData = item.name.toUpperCase();
-       const textData = text.toUpperCase();    
-       return itemData.indexOf(textData) > -1;    
+  searchFilterFunction = text => {
+    const newData = arrayHolder.filter(item => {
+      const itemData = item.name.toUpperCase();
+      const textData = text.toUpperCase();
+      return itemData.indexOf(textData) > -1;
     });
-    setPeople(newData); 
+    setPeople(newData);
   };
 
   return (
     <Container>
-        <Input placeholder="Buscar pelo nome" onChangeText={text => searchFilterFunction(text)} placeholderTextColor="#828282"></Input>
-        <Feather name="search" size={24}></Feather>
+      <Input
+        placeholder="Buscar pelo nome"
+        onChangeText={text => searchFilterFunction(text)}
+        placeholderTextColor="#828282"
+      ></Input>
+      <Feather name="search" size={24}></Feather>
     </Container>
   );
 }
-
